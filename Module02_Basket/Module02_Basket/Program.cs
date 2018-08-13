fb721975ff1e1578e182fddf1c3f397315e8565b
IrynaNikerinaGentlecode <iryna.nikerina@gentlecode.com>
2018-08-13 16:38:27 +0300

    Initial commit

---
+++ Module02_Basket/Module02_Basket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module02_Basket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
