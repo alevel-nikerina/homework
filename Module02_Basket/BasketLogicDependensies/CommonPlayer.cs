08277aaca7a1f24727ae0ad646c96fb2801ee9e4
IrynaNikerinaGentlecode <iryna.nikerina@gentlecode.com>
2018-08-14 12:41:49 +0300

    Basket start logic

---
+++ Module02_Basket/BasketLogicDependensies/CommonPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketLogicDependensies
+{
+	public class CommonPlayer: BasePlayer
+	{
+		public string Type;
+
+		public CommonPlayer(string name, string type) : base(name)
+		{
+			this.Type = type;
+		}
+
+		public override int Attempt()
+		{
+			return randomNumber;
+		}
+	}
+}
---
+++ Module02_Basket/BasketLogicDependensies/NotePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketLogicDependensies
+{
+	class NotePlayer: BasePlayer
+	{
+		public string Type;
+
+		public NotePlayer(string name, string type) : base(name)
+		{
+			this.Type = type;
+		}
+
+		public override int Attempt()
+		{
+			return randomNumber;
+		}
+	}
+}
